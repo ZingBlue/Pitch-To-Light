84e5c6c1db24a73c0e33e0f1afde5610f461d743
ZingBlue <23709367+Hayden4126@users.noreply.github.com>
2020-01-05 20:40:22 -0800

    Minor updates

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
 import aubio
 import numpy
-import flux_led
-# import pyaudio
-print(pitch)
-flux_led.BulbScanner
+from phue import Bridge
+# import pyaudio This library hates me, I'll figure it out eventually
+import wave
+
+b = Bridge('0.0.0.0') # The IP of the light bulb
+
+b.connect() # This only needs to run once, it connects the bulb
+
+
+"""
+arg is lamp number (if you have several lights I think)
+arg2 is the setting changed (in this case brightness)
+arg3 is the value 254 is maximum brightness, 0 is smalliest.
+"""
+b.set_light(1, 'bri', 254)
