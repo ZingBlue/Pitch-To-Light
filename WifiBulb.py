0d043b7d23a72e1a53a198783f75c4479b3de69e
The_VoidSource <extremegamen@gmail.com>
2020-01-08 22:00:55 -0500

    Monkey

--- WifiBulb.py
+++ WifiBulb.py
@@ -1,15 +1,15 @@
-﻿import socket
+import socket
 import sys
 import binascii
 # NOTE: THIS IS NOT MY CODE
 def getChecksumValue(byteArray):
-        """ returns the checksum value as a byte from an array of bytes """
+    """ returns the checksum value as a byte from an array of bytes """
         #sum all the values
-        sumOfValues = 0
-        for x in range(len(byteArray)):
-            sumOfValues += byteArray[x]
+    sumOfValues = 0
+    for x in range(len(byteArray)):
+        sumOfValues += byteArray[x]
         # return
-        return sumOfValues % pow(2, len(byteArray) + 1)
+    return sumOfValues % pow(2, len(byteArray) + 1)
 
 class WifiBulb(object):
     """class for controlling a wifi lightbulb"""
@@ -20,7 +20,7 @@ class WifiBulb(object):
     def __init__(self, IP):
         """ constructor with IP address """
         self.IP = IP
-        self.useDebug = False
+        self.useDebug = True
         self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
     def connect(self):
@@ -51,6 +51,7 @@ class WifiBulb(object):
     def disconnect(self):
         """ disconnects the socket """
         self.s.detach()
+        print ("disconnected from " + self.IP + ":" + str(WifiBulb.PORT))
 
     def warmwhite(self, brightness = 255):
         message = WifiBulb.mode + "000000" + format(brightness, "02x") + "0f0f"
@@ -60,11 +61,11 @@ class WifiBulb(object):
     #     data = "31ffc39200f00f" # 07 39 changes
     #     message = WifiBulb.mode + "ff39ff" + "00f00f"
     #     self._sendmessage(message)
-
+ 
     def setColor(self, color):
         """sets the color the given tuple in the format (R, G, B)"""
-        # print("Sending color: " + str(color))
-        # mode + red + green + blue + magicBytes + checksum
+        print("Sending color: " + str(color))
+        #mode + red + green + blue + magicBytes + checksum
         message = WifiBulb.mode + format(color[0], "02x") + format(color[1], "02x") + format(color[2], "02x") + WifiBulb.magicBytes
         self._sendmessage(message)
 
@@ -77,7 +78,7 @@ if __name__ == "__main__":
     # connect
     bulb.connect()
     # setColor
-    color = (int(sys.argv[2]), int(sys.argv[3]), int(sys.argv[4]))
+    color = (int(sys.argv[1]), int(sys.argv[2]), int(sys.argv[3]))
     bulb.setColor(color)
     # disconnect
     bulb.disconnect()
--- main.py
+++ main.py
@@ -1,23 +1,63 @@
-import aubio
-import numpy
+#import aubio
+#import numpy
+#import webcolors
 from WifiBulb import WifiBulb
 # from phue import Bridge
 # import pyaudio This library hates me, I'll figure it out eventually
 # import flux-led This library is not used rn
-import wave
+import time
 
 # import the class
-from WifiBulb import WifiBulb
 
 # create a new object and assign it's IP address
-IP = "192.168.x.x"
-myLightBulb = WifiBulb(IP)
+IP_king = "192.168.42.20"
+IP_smort = "192.168.42.19"
+right = WifiBulb(IP_king)
+left = WifiBulb(IP_smort)
 
 # connect to the bulb
-myLightBulb.connect()
-
+left.connect()
+right.connect()
+time.sleep(5)
 # send it some colors
-myLightBulb.setColor((255, 0, 0))
+left.setColor((0,0,0))
+right.setColor((0,225,0))
+time.sleep(5)
+left.warmwhite()
+time.sleep(5)
+#time.sleep(2)
+#Preset colors just yeah
 
+#def red(bulb):
+#        bulb.setColor((255,0,0))
+
+#def green(bulb):
+#        bulb.setColor((0,255,0))       
+#def blue(bulb):
+#        bulb.setColor((0,0,225))        
+#def white(bulb):
+#        bulb.warmwhite(255)        
+#color change
+#while(False):
+#    red(left)
+#    green(right)
+#    
+#    time.sleep(5)
+#    
+#    green(left)
+#    blue(right)
+#    
+#    time.sleep(5)
+#    
+#    blue(left)
+#    white(right)
+#    
+#    time.sleep(5)
+#    
+#    white(left)
+#    red(right)
+#    
+#    time.sleep(5)
 # disconnect when finished
-myLightBulb.disconnect()
+right.disconnect()
+left.disconnect()
